5d3ad222f418797b18b38878f67e593cef68dc56
Christian Bell <christiantcbell@gmail.com>
2018-08-27 18:19:23 +0100

    set up the routes and added the gem for stripe but didn't configure it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :arts, only: [ :index, :show, :new, :create, :destroy]
+  resources :commissions do
+    resources :requests, only: [ :new, :create ]
+  end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :requests, only: [ :show]
+  resources :payments, only: [ :show, :new, :create ]
 end
