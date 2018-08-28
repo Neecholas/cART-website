c1aea0b3f065db5d7b63f926f6fd1a4b8c5107bb
Arber Zyb <arber111990@gmail.com>
2018-08-28 18:34:54 +0100

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @test = 'weofbwefb'
+  end
+
   def artist?
     @user = User.find(params[:id])
     @arts.each do |art|
