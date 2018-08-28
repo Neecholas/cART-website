bee3f10147cfd316adade027db45157d4b4512d6
Arber Zyb <arber111990@gmail.com>
2018-08-28 14:28:40 +0100

    def new def create and def params on commission_controller

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -4,4 +4,21 @@ class CommissionsController < ApplicationController
   end
 
 
+  def new
+    @commissions = Commission.new
+  end
+
+  def create
+    @commissions = Commission.new(commisson_params)
+    # @commission.art = Commission.find(params[:art_id])
+    @commission.save
+  end
+
+  private
+
+  def commission_params
+    params.require(:commission).permit(:content)
+    #after completing box update content
+  end
+
 end
