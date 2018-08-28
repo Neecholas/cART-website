b5d7474fc4993893b0c7b93cd6b3412994b94373
Christian Bell <christiantcbell@gmail.com>
2018-08-28 14:45:11 +0100

    added the edit and update functions on the commissions controller

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -7,7 +7,19 @@ class CommissionsController < ApplicationController
     @commission = Commission.find(params[:id])
   end
 
+  def edit
+    @commission = Commission.find(params[:id])
+  end
 
+  def update
+    @commission = Commission.find(params[:id])
+    @commission.update(commission_params)
+    redirect_to commission_path(@commission)
+  end
 
+  private
 
+  def commission_params
+    params.require(:commissions).permit(:title, :description, :amount, :photo)
+  end
 end
