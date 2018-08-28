a8352e1cd518db3af4e6d77cefb3e753ea153478
Christian Bell <christiantcbell@gmail.com>
2018-08-28 12:18:39 +0100

    Have added some seeds to the seed file - one user and five commissions, also created the commissions controller with the index method but yet to do the view

--- db/migrate/20180828094009_create_arts.rb
+++ db/migrate/20180828094009_create_arts.rb
@@ -3,7 +3,7 @@ class CreateArts < ActiveRecord::Migration[5.2]
     create_table :arts do |t|
       t.string :title
       t.string :photo
-      t.references :user_id, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+User.create!(email: "test@test.com", password: "123123")
+
+
+
+puts 'Creating 5 Commissions...'
+5.times do |i|
+  product = Commission.create!(
+    title: Faker::Book.title,
+    description: Faker::BojackHorseman.quote,
+    amount: rand(1..15) * 10,
+    user_id: User.first.id
+  )
+end
+puts 'Finished!'
