d42c0c0242a21660e464fae8831aa7eebbdce0cf
Christian Bell <christiantcbell@gmail.com>
2018-08-29 11:55:52 +0100

    added the user controller index, new and create methods with their respective views in the commission show pages

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,16 +1,30 @@
 class RequestsController < ApplicationController
 
   def index
-    @requests = Request.all
+    @commission = Commission.find(params[:commission_id])
+    @requests = @commission.requests
   end
 
   def new
+    @commission = Commission.find(params[:commission_id])
+    @request = Request.new
   end
 
   def create
+    @commission = Commission.find(params[:commission_id])
+    @request = Request.new(request_params)
+    @request.commission = Commission.find(params[:commission_id])
+    @request.save
+    redirect_to commission_path(@commission)
   end
 
   def delete
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:description, :amount)
   end
 
 end
--- app/models/commission.rb
+++ app/models/commission.rb
@@ -1,5 +1,6 @@
 class Commission < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   belongs_to :user
+  has_many :requests, dependent: :destroy
   validates :title, presence: true
 end
