ae73adb5c49bb3f984e759e84450bb8151cf22a6
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-09-03 22:24:07 +0100

    MAde the show page work the way it should

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_092551) do
+ActiveRecord::Schema.define(version: 2018_09_03_195427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_092551) do
     t.string "last_name"
     t.string "username"
     t.string "bio"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
