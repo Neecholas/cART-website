518bd0f0320de201063862de742f4abba004edc7
Filip Ilinca <filip.ilinca@protonmail.com>
2018-09-06 11:10:28 +0100

    reshaped, repositioned & added some javascript to the sign in/out alert to make it slide away to the right after 3 sec

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let alert = document.querySelector(".alert")
+    if (alert) {
+      setTimeout(() => {
+        alert.style.transition = "all ease 1.5s";
+        alert.style.transform = "translateX(500px)";
+      }, 3000)
+      setTimeout(() => {
+        alert.style.display = "none";
+      }, 3500)
+    }
+  });
